766ea2319e51166cbfffc005edf40ddaf7753f65
nennneko5787 <nennneko5787@gmail.com>
2023-12-13 21:07:25 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -66,7 +66,12 @@ def videodownloader(url: str, svid: int):
 	with YoutubeDL(ydl_opts) as ydl:
 		#ydl.download([url])
 		info_dict = ydl.extract_info(url, download=False)
-		return info_dict
+		flag = "nicovideo" in url
+		if flag == True:
+			ydl.download([url])
+			return "nico"
+		else:
+			return info_dict
 
 
 async def playbgm(voice_client,queue):
@@ -86,12 +91,13 @@ async def playbgm(voice_client,queue):
 	info_dict = videodownloader(url,voice_client.guild.id)
 	logging.info("再生")
 	#voice_client.play(discord.FFmpegPCMAudio(f"{voice_client.guild.id}.mp3"), after=lambda e:playbgm(voice_client, queue))
-	FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-	video_title = info_dict.get('title', None)
-	videourl = info_dict.get('url', None)
-	source = await discord.FFmpegOpusAudio.from_probe(videourl, **FFMPEG_OPTIONS)
 	loop = asyncio.get_event_loop()
-	voice_client.play(source, after=lambda e:loop.create_task(playbgm(voice_client,queue)))
+	if 
+		FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+		video_title = info_dict.get('title', None)
+		videourl = info_dict.get('url', None)
+		source = await discord.FFmpegOpusAudio.from_probe(videourl, **FFMPEG_OPTIONS)
+		voice_client.play(source, after=lambda e:loop.create_task(playbgm(voice_client,queue)))
 	await voice_client.channel.send(f"再生: **{video_title}**")
 
 
