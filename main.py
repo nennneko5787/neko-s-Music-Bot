a8ffb4093a9a2c58a66fd1c9fc58ac2cb6f3d18d
nennneko5787 <nennneko5787@gmail.com>
2023-12-25 16:12:53 +0900

    ChatGPTの言うとおり！

--- main.py
+++ main.py
@@ -77,7 +77,11 @@ def nicodl(url: str, svid: int):
 	with YoutubeDL(ydl_opts) as ydl:
 		ydl.download([url])
 		info_dict = ydl.extract_info(url, download=False)
-		return info_dict
+		# 必要な情報を取り出す処理を追加
+		return {
+			'title': info_dict.get('title', None),
+			'url': info_dict.get('url', None)
+		}
 
 
 async def playbgm(voice_client,queue):
@@ -95,23 +99,22 @@ async def playbgm(voice_client,queue):
 	logging.info("ダウンロードを開始")
 	await voice_client.channel.send(f"再生待機中: **{url}**")
 	loop = asyncio.get_event_loop()
-	if url.find("nicovideo.jp") == -1:
-		info_dict = videodownloader(url,voice_client.guild.id)
-		logging.info("再生")
-		#voice_client.play(discord.FFmpegPCMAudio(f"{voice_client.guild.id}.mp3"), after=lambda e:playbgm(voice_client, queue))
-		FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-		video_title = info_dict.get('title', None)
-		videourl = info_dict.get('url', None)
-		source = await discord.FFmpegOpusAudio.from_probe(videourl, **FFMPEG_OPTIONS)
-		voice_client.play(source, after=lambda e:loop.create_task(playbgm(voice_client,queue)))
-		await voice_client.channel.send(f"再生: **{video_title}**")
-	else:
-		await voice_client.channel.send(f"※ニコニコ動画の動画は再生に少し時間がかかります。ご了承ください。")
-		info_dict = await loop.run_in_executor(None, nicodl(url,voice_client.guild.id))
-		video_title = info_dict.get('title', None)
-		source = discord.FFmpegPCMAudio(f"{voice_client.guild.id}.mp3")
-		voice_client.play(source, after=lambda e:loop.create_task(playbgm(voice_client,queue)))
-	# await voice_client.channel.send(f"DEBUG: **{videourl}**")
+# 修正後の playbgm 関数の一部
+if url.find("nicovideo.jp") == -1:
+	info_dict = videodownloader(url, voice_client.guild.id)
+	logging.info("再生")
+	FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+	video_title = info_dict.get('title', None)
+	videourl = info_dict.get('url', None)
+	source = await discord.FFmpegOpusAudio.from_probe(videourl, **FFMPEG_OPTIONS)
+	voice_client.play(source, after=lambda e: loop.create_task(playbgm(voice_client, queue)))
+	await voice_client.channel.send(f"再生: **{video_title}**")
+else:
+	await voice_client.channel.send(f"※ニコニコ動画の動画は再生に少し時間がかかります。ご了承ください。")
+	info_dict = await loop.run_in_executor(None, lambda: nicodl(url, voice_client.guild.id))
+	video_title = info_dict.get('title', None)
+	source = discord.FFmpegPCMAudio(f"{voice_client.guild.id}.mp3")
+	voice_client.play(source, after=lambda e: loop.create_task(playbgm(voice_client, queue)))
 
 
 @tree.command(name="play", description="音楽を再生します")
