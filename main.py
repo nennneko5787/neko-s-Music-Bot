f42348fd82695c11f00c1e0d7b7131f238204784
nennneko5787 <nennneko5787@gmail.com>
2024-03-17 10:32:34 +0900

    蹴落とすの。

--- main.py
+++ main.py
@@ -74,12 +74,12 @@ YOUTUBE_DISABLED = False
 
 @client.event
 async def setup_hook():
-	print('ログインしました')
 	await tree.set_translator(MyTranslator())
 	await tree.sync()  #スラッシュコマンドを同期
 	
 @client.event
 async def on_ready():
+	print(f'{client.user}にログインしました')
 	myLoop.start()
 	if "Develop" in client.user.name:
 		global YOUTUBE_DISABLED
@@ -181,8 +181,8 @@ async def handle_download_and_play(item, voice_client, channel, language):
 	loop = asyncio.get_event_loop()
 	await asyncio.to_thread(voice_client.play, source, after=lambda e: loop.create_task(playbgm(voice_client, channel, language)))
 	embed = discord.Embed(title="neko's Music Bot", description=await MyTranslator().translate(locale_str("Playing"),language), color=discord.Colour.purple())
-	embed.add_field(name=await MyTranslator().translate(locale_str("Video title"),language), value=title)
-	embed.add_field(name=await MyTranslator().translate(locale_str("Video URL"),language), value=weburl)
+	embed.add_field(name=await MyTranslator().translate(locale_str("title"),language), value=title)
+	embed.add_field(name="URL", value=weburl)
 	embed.set_image(url=thumbnail)
 	await channel.send(embed=embed)
 
@@ -354,7 +354,7 @@ async def handle_music(url, interaction, voice_client=None):
 				"url": info_dict.get('url'),
 				"title": info_dict.get('title') if not isSpotify else result['name'],
 				"id": info_dict.get('id') if not isSpotify else result['id'],
-				"thumbnail": info_dict.get('thumbnail'),
+				"thumbnail": info_dict.get('thumbnail') if not isSpotify else None,
 			})
 			await asyncio.sleep(0.01)
 	else:
@@ -363,7 +363,7 @@ async def handle_music(url, interaction, voice_client=None):
 			"url": dic.get('url'),
 			"title": dic.get('title') if not isSpotify else result['name'],
 			"id": dic.get('id') if not isSpotify else result['id'],
-			"thumbnail": dic.get('thumbnail'),
+			"thumbnail": dic.get('thumbnail') if not isSpotify else None,
 		})
 
 	await send_music_inserted_message(dic, interaction)
@@ -488,6 +488,74 @@ async def resume(interaction: discord.Interaction):
 	embed = discord.Embed(title="neko's Music Bot",description=await MyTranslator().translate(locale_str("Resumed songs that had been paused."),interaction.locale),color=discord.Colour.purple())
 	await interaction.response.send_message("",embed=embed)
 
+class QueueView(discord.ui.View):
+	def __init__(self):
+		super().__init__(timeout=None)
+		self.page = 0
+	
+	@discord.ui.button(emoji="◀", label=locale_str("Prev page"), style=discord.ButtonStyle.success)
+	async def prev(self, interaction: discord.Interaction):
+		if interaction.guild.id in queue_dict:
+			await interaction.response.defer()
+			self.page -= 1
+			q = copy.deepcopy(queue_dict[interaction.guild.id])
+			qlist = []
+			c = 1
+			if nowPlaying_dict[f"{interaction.guild.id}"].get("title",None) is not None:
+				qlist.append(f"**{await MyTranslator().translate(locale_str("Playing"),interaction.locale)}: **[{nowPlaying_dict[f'{interaction.guild.id}'].get('title')}]({nowPlaying_dict[f'{interaction.guild.id}'].get('webpage_url')})")
+			else:
+				qlist.append(f"**{await MyTranslator().translate(locale_str("Playing"),interaction.locale)}: **None")
+			# キューの中身を表示
+			while not q.empty():
+				item = await q.get()
+				if c >= self.page * 10 and c <= self.page + 10:
+					qlist.append(f"#{c} [{item.get('title')}]({item.get('webpage_url')})")
+				c = c + 1
+				await asyncio.sleep(0.01)
+			embed = discord.Embed(title="neko's Music Bot", description="\n".join(qlist), color=discord.Colour.purple())
+			view = QueueView()
+			if self.page == 1:
+				view.prev.disabled = True
+			await interaction.message.edit(embed=embed, view=view)
+		else:
+			embed = discord.Embed(title="neko's Music Bot",description=await MyTranslator().translate(locale_str("No songs in queue"),interaction.locale),color=discord.Colour.red())
+			await interaction.response.send_message(embed=embed, ephemeral=True)
+			return
+
+	@discord.ui.button(emoji="▶", label=locale_str("Next page"), style=discord.ButtonStyle.success)
+	async def next(self, interaction: discord.Interaction):
+		if interaction.guild.id in queue_dict:
+			await interaction.response.defer()
+			self.page += 1
+			q = copy.deepcopy(queue_dict[interaction.guild.id])
+			qlist = []
+			c = 1
+			if nowPlaying_dict[f"{interaction.guild.id}"].get("title",None) is not None:
+				qlist.append(f"**{await MyTranslator().translate(locale_str("Playing"),interaction.locale)}: **[{nowPlaying_dict[f'{interaction.guild.id}'].get('title')}]({nowPlaying_dict[f'{interaction.guild.id}'].get('webpage_url')})")
+			else:
+				qlist.append(f"**{await MyTranslator().translate(locale_str("Playing"),interaction.locale)}: **None")
+			# キューの中身を表示
+			while not q.empty():
+				item = await q.get()
+				if c >= self.page * 10 and c <= self.page + 10:
+					qlist.append(f"#{c} [{item.get('title')}]({item.get('webpage_url')})")
+				c = c + 1
+				await asyncio.sleep(0.01)
+			embed = discord.Embed(title="neko's Music Bot", description="\n".join(qlist), color=discord.Colour.purple())
+			view = QueueView()
+			if (self.page + 1)*10 > c:
+				view.next.disabled = True
+			await interaction.message.edit(embed=embed, view=view)
+		else:
+			embed = discord.Embed(title="neko's Music Bot",description=await MyTranslator().translate(locale_str("No songs in queue"),interaction.locale),color=discord.Colour.red())
+			await interaction.response.send_message(embed=embed, ephemeral=True)
+			return
+
+	@discord.ui.button(emoji="❌", label=locale_str("Close queue view"), style=discord.ButtonStyle.danger)
+	async def close(self, interaction: discord.Interaction):
+		await interaction.response.defer()
+		await interaction.message.delete()
+
 @tree.command(name="queue", description=locale_str("You can check the songs in the queue."))
 @discord.app_commands.guild_only()
 async def queue(interaction: discord.Interaction):
@@ -497,17 +565,20 @@ async def queue(interaction: discord.Interaction):
 		qlist = []
 		c = 1
 		if nowPlaying_dict[f"{interaction.guild.id}"].get("title",None) is not None:
-			qlist.append(f"**現在再生中: **[{nowPlaying_dict[f'{interaction.guild.id}'].get('title')}]({nowPlaying_dict[f'{interaction.guild.id}'].get('webpage_url')})")
+			qlist.append(f"**{await MyTranslator().translate(locale_str("Playing"),interaction.locale)}: **[{nowPlaying_dict[f'{interaction.guild.id}'].get('title')}]({nowPlaying_dict[f'{interaction.guild.id}'].get('webpage_url')})")
 		else:
-			qlist.append(f"**現在再生中: **None")
+			qlist.append(f"**{await MyTranslator().translate(locale_str("Playing"),interaction.locale)}: **None")
 		# キューの中身を表示
 		while not q.empty():
 			item = await q.get()
-			qlist.append(f"#{c} [{item.get('title')}]({item.get('webpage_url')})")
+			if c >= 10 and c <= 20:
+				qlist.append(f"#{c} [{item.get('title')}]({item.get('webpage_url')})")
 			c = c + 1
 			await asyncio.sleep(0.01)
 		embed = discord.Embed(title="neko's Music Bot", description="\n".join(qlist), color=discord.Colour.purple())
-		await interaction.followup.send(embed=embed)
+		view = QueueView()
+		view.prev.disabled = True
+		await interaction.followup.send(embed=embed, view=view)
 	else:
 		embed = discord.Embed(title="neko's Music Bot",description=await MyTranslator().translate(locale_str("No songs in queue"),interaction.locale),color=discord.Colour.red())
 		await interaction.response.send_message(embed=embed, ephemeral=True)
--- translate.py
+++ translate.py
@@ -38,6 +38,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "ゲートウェイのPing、CPU使用率、メモリ使用率を見ることができます。",
 		"The feature to play Youtube songs has been disabled.": "Youtubeの曲を再生する機能は無効化されています。",
 		"Displays an invitation link to the support server.": "サポートサーバーの招待リンクを表示します。",
+		"Prev page": "前のページ",
+		"Next page": "次のページ",
+		"Close queue view": "キューを閉じる",
 	},
 	Locale.chinese: {
 		'Plays the music specified by url. If music is already being played, it is inserted into the cue.': '播放指定的音乐。如果已经在播放音乐，则将其插入到队列中。',
@@ -76,6 +79,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "您可以查看网关的 Ping、CPU 使用率和内存使用率。",
 		"The feature to play Youtube songs has been disabled.": "已禁用播放Youtube歌曲的功能。",
 		"Displays an invitation link to the support server.": "显示支持服务器的邀请链接。",
+		"Prev page": "上一页",
+		"Next page": "下一页",
+		"Close queue view": "关闭队列视图",
 	},
 	Locale.taiwan_chinese: {
 		'Plays the music specified by url. If music is already being played, it is inserted into the cue.': '播放指定的音樂URL。如果已經在播放音樂，則將其插入到佇列中。',
@@ -114,6 +120,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "您可以查看閘道器的 Ping、CPU 使用率和記憶體使用率。",
 		"The feature to play Youtube songs has been disabled.": "已停用播放 Youtube 歌曲的功能。",
 		"Displays an invitation link to the support server.": "顯示支援伺服器的邀請鏈接。",
+		"Prev page": "上一頁",
+		"Next page": "下一頁",
+		"Close queue view": "關閉佇列檢視"
 	},
 	Locale.korean: {
 		'Plays the music specified by url. If music is already being played, it is inserted into the cue.': '지정된 URL의 음악을 재생합니다. 이미 음악이 재생 중인 경우에는 큐에 삽입됩니다.',
@@ -151,6 +160,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "게이트웨이의 핑(Ping), CPU 사용률 및 메모리 사용률을 확인할 수 있습니다.",
 		"The feature to play Youtube songs has been disabled.": "YouTube 노래를 재생하는 기능이 비활성화되었습니다.",
 		"Displays an invitation link to the support server.": "지원 서버로의 초대 링크를 표시합니다.",
+		"Prev page": "이전 페이지",
+		"Next page": "다음 페이지",
+		"Close queue view": "대기열 보기 닫기"
 	},
 	Locale.ukrainian: {
 		'Plays the music specified by url. If music is already being played, it is inserted into the cue.': 'Відтворює музику, вказану за URL. Якщо музика вже відтворюється, вона вставляється в чергу.',
@@ -188,6 +200,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "Ви можете переглядати пінг, використання ЦП та використання пам'яті відомого шлюзу.",
 		"The feature to play Youtube songs has been disabled.": "Функція відтворення пісень з YouTube була вимкнена.",
 		"Displays an invitation link to the support server.": "Показує посилання на запрошення до сервера підтримки.",
+		"Prev page": "Попередня сторінка",
+		"Next page": "Наступна сторінка",
+		"Close queue view": "Закрити перегляд черги"
 	},
 	Locale.russian: {
 		'Plays the music specified by url. If music is already being played, it is inserted into the queue.': 'Воспроизводит музыку, указанную по URL. Если музыка уже играет, она вставляется в очередь.',
@@ -225,6 +240,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "Вы можете просматривать пинг, использование ЦП и использование памяти шлюза.",
 		"The feature to play Youtube songs has been disabled.": "Функция воспроизведения песен с YouTube отключена.",
 		"Displays an invitation link to the support server.": "Отображает пригласительную ссылку на сервер поддержки.",
+		"Prev page": "Предыдущая страница",
+		"Next page": "Следующая страница",
+		"Close queue view": "Закрыть просмотр очереди"
 	},
 	Locale.vietnamese: {
 		'Plays the music specified by url. If music is already being played, it is inserted into the cue.': 'Phát nhạc được chỉ định bằng url. Nếu nhạc đã được phát, nó sẽ được chèn vào hàng đợi.',
@@ -262,6 +280,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "Xem Ping cổng, sử dụng CPU và sử dụng bộ nhớ.",
 		"The feature to play Youtube songs has been disabled.": "Chức năng phát nhạc từ Youtube đã bị vô hiệu hóa.",
 		"Displays an invitation link to the support server.": "Hiển thị liên kết mời đến máy chủ hỗ trợ.",
+		"Prev page": "Trang trước",
+		"Next page": "Trang tiếp theo",
+		"Close queue view": "Đóng xem hàng đợi"
 	},
 	Locale.thai: {
 		'Plays the music specified by url. If music is already being played, it is inserted into the cue.': 'เล่นเพลงที่ระบุโดย url หากเพลงกำลังเล่นอยู่แล้ว จะถูกแทรกเข้าไปในคิว',
@@ -299,6 +320,9 @@ TLANSLATION_DATA: dict[Locale, dict[str, str]]= {
 		"View gateway Ping, CPU utilization, and memory utilization.": "ดูปิงเกตเวย์ การใช้ CPU และการใช้หน่วยความจำ",
 		"The feature to play Youtube songs has been disabled.": "คุณลักษณะที่ใช้เล่นเพลง Youtube ถูกปิดใช้งาน",
 		"Displays an invitation link to the support server.": "แสดงลิงก์เชิญไปยังเซิร์ฟเวอร์สนับสนุน",
+		"Prev page": "หน้าก่อนหน้า",
+		"Next page": "หน้าถัดไป",
+		"Close queue view": "ปิดมุมมองคิว"
 	},
 }
 
