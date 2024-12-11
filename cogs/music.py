24f138e86d66fa96a3a5fd996121c9671bf12897
ねんねこ <nennneko5787@gmail.com>
2024-12-11 22:11:53 +0900

    music.py を更新

--- cogs/music.py
+++ cogs/music.py
@@ -51,10 +51,12 @@ class MusicCog(commands.Cog):
                     self.source[guild.id] = await NicoNicoSource.from_url(
                         info["url"], self.niconico, info["volume"]
                     )
+                    print("a")
                 else:
                     self.source[guild.id] = await YTDLSource.from_url(
                         info["url"], info["volume"]
                     )
+                    print("b")
 
         await get()
 
