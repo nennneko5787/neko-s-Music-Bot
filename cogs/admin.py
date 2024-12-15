3438e01271d141b1e677fd2b0aa5f1562ca157c4
ねんねこ <nennneko5787@gmail.com>
2024-12-16 01:27:38 +0900

    Create admin.py

---
+++ cogs/admin.py
@@ -0,0 +1,9 @@
+import discord
+from discord.ext import commands
+
+class AdminCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+async def setup(bot: commands.Bot):
+    await bot.add_cog(AdminCog(bot))
