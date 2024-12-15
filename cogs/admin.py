ac2b893fa9979990ee46441aec4db31a97b45ac3
ねんねこ <nennneko5787@gmail.com>
2024-12-16 01:29:32 +0900

    admin.py を更新

--- cogs/admin.py
+++ cogs/admin.py
@@ -5,5 +5,10 @@ class AdminCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         
+    @commands.command(name="guild")
+    async def guildCommand(self, ctx: commands.Context, guildId: int):
+        guild = self.bot.get_guild(guildId)
+        await ctx.reply(guild.name)
+        
 async def setup(bot: commands.Bot):
     await bot.add_cog(AdminCog(bot))
