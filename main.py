347d7461a03c12f5b38d3ca40c27020f8928867c
ねんねこ <nennneko5787@gmail.com>
2024-12-16 01:32:31 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from discord.ext import commands
 
 dotenv.load_dotenv()
 
-intents = discord.Intents.none()
-intents.guilds = True
-intents.voice_states = True
+intents = discord.Intents.default()
 
 
 bot = commands.Bot(
