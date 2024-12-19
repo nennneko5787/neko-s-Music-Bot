f29aab5970fb3470338dd4cb96ddc8dc901097ae
Nennneko5787 <nennneko5787@gmail.com>
2024-12-19 16:29:23 +0900

    !?

--- cogs/admin.py
+++ cogs/admin.py
@@ -3,13 +3,14 @@ import traceback
 import discord
 from discord.ext import commands
 
+
 class AdminCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
+
     @commands.command(name="sync")
     async def syncCommand(self, ctx: commands.Context):
-        if ctx.author.id == 1048448686914551879:
+        if ctx.author.id != 1048448686914551879:
             return
         try:
             await self.bot.tree.sync()
@@ -17,6 +18,7 @@ class AdminCog(commands.Cog):
         except:
             traceback.print_exc()
             await ctx.reply("ng")
-        
+
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(AdminCog(bot))
