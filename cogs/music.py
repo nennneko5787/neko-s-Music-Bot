0bb2d41108ca871335a8e7c7bef6a1ed8d056604
ねんねこ <nennneko5787@gmail.com>
2024-12-24 00:08:47 +0900

    music.py を更新

--- cogs/music.py
+++ cogs/music.py
@@ -420,9 +420,9 @@ class MusicCog(commands.Cog):
             embed.colour = discord.Colour.greyple()
             embed.set_author(name="再生終了")
         elif voiceClient.is_playing() or voiceClient.is_paused():
-            bar = discord.utils.get(self.bot.emojis, name="bar")
-            circle = discord.utils.get(self.bot.emojis, name="circle")
-            graybar = discord.utils.get(self.bot.emojis, name="graybar")
+            bar = str(discord.utils.get(self.bot.emojis, name="bar"))
+            circle = str(discord.utils.get(self.bot.emojis, name="circle"))
+            graybar = str(discord.utils.get(self.bot.emojis, name="graybar"))
 
             percentage = source.progress / source.info["duration"]
             barLength = 15
